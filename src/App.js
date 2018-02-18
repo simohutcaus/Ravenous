b9810cea1885ca59b4aa8cbda0d6ccc54a8eb281
Simon Hutchinson <simonhutc@gmail.com>
2018-02-18 20:10:22 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BusinessList from '../src/components/BusinessList/BusinessList';
+import Business from '../src/components/Business/Business';
+import SearchBar from '../src/components/SearchBar/SearchBar';
+
+let business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+
+let businesses = [business, business, business, business, business, business];
+
+
+class App extends Component {
+
+  searchYelp(term, location, sortBy) {
+    console.log(term + ' ' + location + ' ' + sortBy);
+  }
+  render() {
+    return (
+      <div class="App">
+  <h1>ravenous</h1>
+  <SearchBar searchYelp={this.searchYelp} />
+  <BusinessList businesses={businesses} />
+</div>
+    );
+  }
+}
+
+export default App;
